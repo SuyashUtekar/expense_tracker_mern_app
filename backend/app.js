fa351e56d0598d468709c60bd488c03131f38eff
SuyashUtekar <minecraftzannds@gmail.com>
2024-06-29 21:26:15 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { db } = require("./db/db.js");
+const { readdirSync } = require("fs");
 const app = express();
 
 require("dotenv").config();
@@ -10,11 +12,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+//routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
 const server = () => {
+  db();
   app.listen(PORT, () => {
     console.log("Listening to port: ", PORT);
   });
